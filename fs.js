1187eb2c62ea07849a5504410ca0458a4803fb04
Nabaraj <nabaraj.dhl77@gmail.com>
2022-07-05 13:17:56 +0545

    fs async and sync practice

---
+++ fs.js
@@ -0,0 +1,11 @@
+// const fs = require("fs");
+// fs.writeFileSync("./first.txt", "Hello Nabaraj");
+// let data = fs.readFileSync("./first.txt", "utf-8");
+// console.log(data);
+// fs.appendFileSync("./first.txt", " How are you?");
+// fs.appendFileSync("./first.txt", " Where are you from?");
+
+//CHALLENGE
+const fs = require("fs");
+fs.mkdirSync("./Naba");
+fs.writeFileSync("Naba/naba.txt", "Hello Nabaraj");
